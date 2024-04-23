1af31f96ba336dc577c688ad85ec8d28bb0ffe53
didapatria <didapatria3@gmail.com>
2024-04-23 13:21:17 +0700

    FINISH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { ProductCardModel } from "@/model/product-card.model";
 const Home = () => {
   const products = ProductCardModel.data;
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto space-y-6 p-4">
       <h1 className="text-center text-4xl font-bold md:text-left">
         Product Card
       </h1>
-      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {products.map((product) => {
           return <ProductCard key={product.id} {...product} />;
         })}
